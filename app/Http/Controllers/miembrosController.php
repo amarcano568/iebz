06dd96eb2c6b06bf4e0c0730e71fc732a279ab3a
amarcano568 <amarcano568@gmail.com>
2020-05-13 09:53:20 +0200

    Nuevas modificaciones

--- app/Http/Controllers/miembrosController.php
+++ app/Http/Controllers/miembrosController.php
@@ -50,7 +50,14 @@ class miembrosController extends Controller
     public function listarMiembros()
     {
 
-        $miembros = \App\Miembros::join('iglesias','miembros.idIglesia','=','iglesias.id')->join('comunidades','miembros.comunidad','=','comunidades.id')->join('provincias','miembros.provincia','=','provincias.id')->join('paises','miembros.paisNacimiento','=','paises.id')->join('profesiones','miembros.profesion','=','profesiones.id')->join('status','miembros.status','=','status.id')->select('miembros.id', 'miembros.idIglesia', 'miembros.nombre', 'miembros.apellido1', 'miembros.apellido2', 'miembros.tipoDocumento','miembros.nroDocumento', 'miembros.direccion', 'miembros.codigoPostal', 'miembros.comunidad', 'miembros.provincia', 'miembros.telefonoFijo', 'miembros.telefonoMovil', 'miembros.email', 'miembros.fecNacimiento', 'miembros.lugarNacimiento', 'miembros.paisNacimiento', 'miembros.profesion', 'miembros.fecBautismo', 'miembros.iglesiaBautismo', 'miembros.fecCartaTraslado', 'miembros.iglesiaProcedencia', 'miembros.otrosDatos', 'miembros.foto', 'miembros.relacionFamilia', 'miembros.status', 'miembros.poblacion','iglesias.nombreCorto','comunidades.nombre AS comunidad','provincias.nombre AS provincia','paises.nombre AS pais','profesiones.nombre AS profesion','status.nombre AS statusNombre')->get();
+        $miembros = \App\Miembros::join('iglesias','miembros.idIglesia','=','iglesias.id')
+            ->join('comunidades','miembros.comunidad','=','comunidades.id')
+            ->join('provincias','miembros.provincia','=','provincias.id')
+            ->join('paises','miembros.paisNacimiento','=','paises.id')
+            ->leftjoin('profesiones','miembros.profesion','=','profesiones.id')
+            ->join('status','miembros.status','=','status.id')
+            ->select('miembros.id', 'miembros.idIglesia', 'miembros.nombre', 'miembros.apellido1', 'miembros.apellido2', 'miembros.tipoDocumento','miembros.nroDocumento', 'miembros.direccion', 'miembros.codigoPostal', 'miembros.comunidad', 'miembros.provincia', 'miembros.telefonoFijo', 'miembros.telefonoMovil', 'miembros.email', 'miembros.fecNacimiento', 'miembros.lugarNacimiento', 'miembros.paisNacimiento', 'miembros.profesion', 'miembros.fecBautismo', 'miembros.iglesiaBautismo', 'miembros.fecCartaTraslado', 'miembros.iglesiaProcedencia', 'miembros.otrosDatos', 'miembros.foto', 'miembros.relacionFamilia', 'miembros.status', 'miembros.poblacion','iglesias.nombreCorto','comunidades.nombre AS comunidad','provincias.nombre AS provincia','paises.nombre AS pais','profesiones.nombre AS profesion','status.nombre AS statusNombre')
+            ->get();
         $miembros->map(function($miembro){           
             $edad = Carbon::parse($miembro->fecNacimiento)->age;
             $miembro->edad = $edad;
