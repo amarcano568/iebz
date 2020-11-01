51ee8c8a335c6257cec91a4dd802db5df514a370
amarcano568 <amarcano568@gmail.com>
2020-11-01 16:19:28 +0100

    Nuevo reporte por nacionalidades

--- app/Http/Controllers/informesController.php
+++ app/Http/Controllers/informesController.php
@@ -131,7 +131,9 @@ class informesController extends Controller
 
         $miembros = \App\Miembros::where(['idIglesia' => $request->idIglesia, 'miembros.status' => $request->status])
             ->join('paises','miembros.paisNacimiento','paises.id')
-            ->select('paises.nombre as paisNombre','miembros.id', 'miembros.telefonoFijo', 'miembros.telefonoMovil', 'miembros.email', 'miembros.idIglesia', 'miembros.nombre', 'miembros.apellido1', 'miembros.apellido2', 'miembros.fecNacimiento', DB::raw('TIMESTAMPDIFF(YEAR,miembros.fecNacimiento,CURDATE()) AS edad'))->get();
+            ->select('paises.nombre as paisNombre','miembros.id', 'miembros.telefonoFijo', 'miembros.telefonoMovil', 'miembros.email', 'miembros.idIglesia', 'miembros.nombre', 'miembros.apellido1', 'miembros.apellido2', 'miembros.fecNacimiento', DB::raw('TIMESTAMPDIFF(YEAR,miembros.fecNacimiento,CURDATE()) AS edad'))
+            ->orderBy('paisNombre', 'Asc')
+            ->get();
         $total = count($miembros);
 
         $data = array(
