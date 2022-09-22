85ea6eba1a1271c81754ec34f25c15836c488a42
Alexander Marcano A <amarcano568@gmail.com>
2022-09-22 18:04:39 +0200

    PRUEBA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,6 @@
     
     </head>
     <body class="nav-md">
-        
         <div class="container body">
             <div class="main_container">
                 <div class="col-md-3 left_col">
